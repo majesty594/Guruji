5f2359edaf386bb85c5bebe30727a336d59d1b01
Majesty Raj <majestyraj@gmnail.com>
2021-06-13 20:18:34 +0530

    date time function

--- assisstant.py
+++ assisstant.py
@@ -1,5 +1,5 @@
 import pyttsx3
-
+import datetime
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
@@ -12,3 +12,19 @@ def speak(audio):
     engine.runAndWait()
     
 #speak("Hi I am Guruji your virtual assistant")
+
+def time():
+    Time = datetime.datetime.now().strftime("%h:%M:%S")
+    speak(Time)
+#time()
+
+def date():
+    year = int(datetime.datetime.now().year)
+    month = int(datetime.datetime.now().month)
+    date =int(datetime.datetime.now().day)
+    speak("The Current date is")
+    speak(date)
+    speak(month)
+    speak(year)
+
+#date()
